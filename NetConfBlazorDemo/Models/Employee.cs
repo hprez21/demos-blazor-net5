ef23618017c88c5dcceee5d449ce601e3ba67db1
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2020-11-15 12:33:51 -0600

    Initial Commit
    Initial Commit

---
+++ NetConfBlazorDemo/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetConfBlazorDemo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
